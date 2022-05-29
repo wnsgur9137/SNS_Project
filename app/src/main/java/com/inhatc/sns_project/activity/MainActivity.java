5183e684e322ca737a4e33da6252a1e72d05426c
Junhyeok <83490421+wnsgur9137@users.noreply.github.com>
2022-05-30 02:34:02 +0900

    profile gallery_01

--- app/src/main/java/com/inhatc/sns_project/activity/MainActivity.java
+++ app/src/main/java/com/inhatc/sns_project/activity/MainActivity.java
@@ -1,11 +1,9 @@
-package com.inhatc.sns_project;
+package com.inhatc.sns_project.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Camera;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,10 +12,10 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserInfo;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.inhatc.sns_project.R;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
