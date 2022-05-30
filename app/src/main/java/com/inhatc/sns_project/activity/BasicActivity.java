42f2b5ce4fa6922f2165a91cbcdc09d551f0ff75
Junhyeok <83490421+wnsgur9137@users.noreply.github.com>
2022-05-30 17:52:59 +0900

    board insert xml

---
+++ app/src/main/java/com/inhatc/sns_project/activity/BasicActivity.java
@@ -0,0 +1,14 @@
+package com.inhatc.sns_project.activity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BasicActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+}
